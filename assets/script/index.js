df389d8e958f6fda85f692cf67c3eb54fe625c05
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-07-01 22:39:13 +0700

    memperbaiki navbar, dropdown, dan search

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,10 +1,22 @@
-//! Action ketika list navbar di klik
+//! Action ketika list navbar di klik maka keluar dropdown
 const listNavbar = document.querySelectorAll('.li-navbar');
 
 listNavbar.forEach((navbar) => {
-  const dropdown = navbar.querySelector('.dropdown');
+    const dropdown = navbar.querySelector('.dropdown');
 
-  navbar.addEventListener('click', () => {
-    dropdown.classList.toggle('active');
-  });
+    navbar.addEventListener('click', () => {
+        dropdown.classList.toggle('active');
+    });
+
 });
+
+//! ketika user klik di luar dropdown (menutup dropdown)
+document.addEventListener('click', (event) => {
+    const dropdowns = document.querySelectorAll('.dropdown');
+  
+    dropdowns.forEach((dropdown) => {
+      if (!dropdown.parentNode.contains(event.target)) {
+        dropdown.classList.remove('active');
+      }
+    });
+  });
